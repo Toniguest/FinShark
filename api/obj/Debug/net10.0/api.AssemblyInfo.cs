d3d9514f305dddd66dbd9a618ebbe5f9dae42510
Toniguest <combo2100@gmail.com>
2026-02-09 14:36:51 +0100

    Update AssemblyInformationalVersion and cache inputs for build consistency

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15968acbe4c17ab164ea568de8a7ba40f02b4894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209c1490c0261becea677a6142012ad3932316c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
